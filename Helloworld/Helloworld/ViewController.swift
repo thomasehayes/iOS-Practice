3143c38424851aa471bee94d272ee4f91f7a9387
Thomas Hayes <thomashayes7889@gmail.com>
2017-07-31 15:46:44 -0500

    Hello World iPhone App

---
+++ Helloworld/Helloworld/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Helloworld
+//
+//  Created by Thomas Hayes on 7/31/17.
+//  Copyright © 2017 Thomas Hayes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var background: UIImageView!
+    
+    @IBOutlet weak var titleImage: UIImageView!
+    
+    @IBOutlet weak var welcomeBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func welcomePressed(_ sender: Any) {
+        background.isHidden = false
+        titleImage.isHidden = false
+        welcomeBtn.isHidden = true
+    }
+
+}
+
